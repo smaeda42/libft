6d48beafcd02de04dc3cc623767b92fcd60b12ca
Shigeyuki Maeda <smaeda@c1r7s4.42tokyo.jp>
2024-08-22 21:29:36 +0900

    0822

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: smaeda <smaeda@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 16:21:54 by smaeda            #+#    #+#             */
-/*   Updated: 2024/07/11 22:32:03 by smaeda           ###   ########.fr       */
+/*   Updated: 2024/08/22 21:26:00 by smaeda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,14 +35,17 @@ char	**ft_split(char const *s, char c)
 	size_t	word_len;
 	size_t	word_count;
 	int		i;
+	int		j;
 
 	if (!s || !c)
 		return (NULL);
 	word_count = ft_word_count(s, c);
-	list = (char **)malloc(sizeof(char) * (word_count + 1));
+//	list = (char **)malloc(sizeof(char *) * (word_count + 1));
+	list = ft_calloc((ft_word_count(s, c) + 1), sizeof(char *));
 	if (!list)
 		return (NULL);
 	i = 0;
+	j = 0;
 	while (s[i])
 	{
 		while (s[i] == c && s[i])
@@ -53,29 +56,29 @@ char	**ft_split(char const *s, char c)
 				word_len = ft_strlen(&s[i]);
 			else 
 				word_len = ft_strchr(&s[i], c) - &s[i];
-			list[i] = ft_substr(&s[i], 0, word_len);
-			if (!list[i])
+			list[j] = ft_substr(&s[i], 0, word_len);
+			if (!list[j])
 			{
-				while(i >= 0)
+				while(j >= 0)
 				{
-					free(list[i]);
-					i--;
+					free(list[j]);
+					j--;
 				}
 				free(list);
 				return (NULL);
 			}
-			i++;
-			s += word_len;
+			j++;
+			i += word_len;
 		}
 	}
-	list[i] = 0;
+	list[j] = 0;
 	return (list);
 }
 
 int	main()
 {
-	char	str1[] = "lorem lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse";
-	char	c1 = ' ';
+	char	str1[] = "\0aa\0bbb";
+	char	c1 = '\0';
 	char	**lst;
 	lst = ft_split(str1, c1);
 }
