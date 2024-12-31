29f56986c1bf2a635bd7714578e7c360f13d9b33
Shigeyuki Maeda <smaeda@c2r2s2.42tokyo.jp>
2024-12-31 16:59:07 +0900

    20241231

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -12,12 +12,6 @@
 
 int	ft_isalnum(int c)
 {
-	if (c >= '0' && c <= '9')
-		return (1);
-	else if (c >= 'A' && c <= 'Z')
-		return (1);
-	else if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
+	
+	return (ft_isalpha(c) || ft_isdigit(c));
 }
